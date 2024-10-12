6f79f5f2523e39bb2b3a356d19fd61f3809c1d82
H008Harshit <harshitmarmat01@gmail.com>
2024-10-12 18:40:23 +0530

    different type of http method and query params

--- src/app.js
+++ src/app.js
@@ -3,14 +3,35 @@ const express = require('express');
 const app = express();
 
 
-app.use('/test',(req,res)=> {
-    res.end('Testing');
+app.get('/user/:user_id',(req,res)=>{
+    console.log(req.params);
+    console.log(req.query);
+    res.send("Get User data")
 })
 
-app.use("/hello",(req,res)=> {
-    res.end("Hey from dev");
+app.post("/user",(req,res)=>{
+    res.send("Post User data")
 })
 
-app.listen(7777,()=> {
+app.patch("/user",(req,res)=>{
+    res.send("Patch User data");
+})
+
+app.delete("/user",(req,res)=> {
+    res.send("Delete User data");
+})
+
+// app.use("/test2",(req,res)=> {
+//     res.end("test2")
+// })
+// app.use('/test',(req,res)=> {
+//     res.end('Testing');
+// })
+
+// app.use("/hello",(req,res)=> {
+//     res.end("Hey from dev");
+// })
+
+app.listen("7777",()=> {
     console.log("Listening server on port 7777...");
 });
