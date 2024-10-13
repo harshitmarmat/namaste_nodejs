95d3d5c750556704c2ed74640accb1999f44220e
H008Harshit <harshitmarmat01@gmail.com>
2024-10-13 23:40:52 +0530

    played with next()

--- src/app.js
+++ src/app.js
@@ -1,37 +1,26 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
+app.use(
+  "/user",
+  (req, res, next) => {
+    next();
+    console.log("req 1");
+    // res.end("Request Handler 1!!");
+    }
+    ,
+    (req, res, next) => {
+        console.log("RH 2");
+        next();
+    }
+    ,
+    (req, res, next) => {
+        console.log("RH 3");
+        next();
+    }
+);
 
-app.get('/user/:user_id',(req,res)=>{
-    console.log(req.params);
-    console.log(req.query);
-    res.send("Get User data")
-})
-
-app.post("/user",(req,res)=>{
-    res.send("Post User data")
-})
-
-app.patch("/user",(req,res)=>{
-    res.send("Patch User data");
-})
-
-app.delete("/user",(req,res)=> {
-    res.send("Delete User data");
-})
-
-// app.use("/test2",(req,res)=> {
-//     res.end("test2")
-// })
-// app.use('/test',(req,res)=> {
-//     res.end('Testing');
-// })
-
-// app.use("/hello",(req,res)=> {
-//     res.end("Hey from dev");
-// })
-
-app.listen("7777",()=> {
-    console.log("Listening server on port 7777...");
+app.listen("7777", () => {
+  console.log("Listening server on port 7777...");
 });
