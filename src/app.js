1662d10a7bf4864bdaa3d4a272e908fa303973f0
Harshit <h008@Harshits-MacBook-Air.local>
2024-10-17 19:35:30 +0530

    added CRUD apis

--- src/app.js
+++ src/app.js
@@ -1,23 +1,63 @@
 const express = require("express");
 const { AdminAuth, UserAuth } = require("./middlewares/auth");
 const { connectDb } = require("./config/database");
-const  User  = require("./models/User");
+const User = require("./models/User");
 const app = express();
 
+app.use(express.json());
+
+// get user data via email
+app.get("/user", async (req, res) => {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    if (!user) {
+      res.status(404).send("User not found");
+    } else {
+      res.send(user);
+    }
+  } catch (err) {
+    res.status(400).send("Something went wrong");
+  }
+});
+
+// get all user data
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.send(users);
+  } catch (err) {
+    res.status(400).send("Something went wrong");
+  }
+});
+
+// creating user
 app.post("/signup", async (req, res) => {
-  const user = new User({
-    firstName: "Anshul",
-    lastName: "gupta",
-    age: 20,
-    email: "anshul@gupta.com",
-  });
-  try{
+  const user = new User(req.body);
+  try {
     await user.save();
     res.send("User created Successfully");
-  }
-  catch(err){
+  } catch (err) {
     console.error("Error in creating User", err);
-    res.status(400).send("Error in creating User", err.message)
+    res.status(400).send("Error in creating User", err.message);
+  }
+});
+
+app.delete("/user", async (req, res) => {
+  try {
+    await User.findByIdAndDelete(req.body.userId);
+    res.send("User deleted successfully.");
+  } catch (err) {
+    res.status(400).send("Something went Wrong", err.message);
+  }
+});
+
+app.patch("/user", async (req, res) => {
+  try {
+    const { userEmail } = req.body;
+    await User.findOneAndUpdate({"email":userEmail},req.body,{strict:true});
+    res.send("User updated successfully.");
+  } catch (err) {
+    res.status(400).send("Something went Wrong", err.message);
   }
 });
 
