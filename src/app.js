5f750876a331675f02c5dc58c3eaa932aeee7c09
Harshit <h008@Harshits-MacBook-Air.local>
2024-10-21 01:35:58 +0530

    added Schema helper function

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ const { validateSignUpData } = require("./utils/validate");
 const cookieParser = require("cookie-parser");
 const jwt = require("jsonwebtoken");
 const app = express();
-
+const {userAuth} = require("./utils/auth")
 app.use(express.json());
 app.use(cookieParser());
 // creating user
@@ -36,11 +36,10 @@ app.post("/login", async (req, res) => {
     const { email, password } = req.body;
     const user = await User.findOne({ email: email });
     if (!user) throw new Error("Invalid Password.");
-    const isPasswordValid = await bcrypt.compare(password, user.password);
-
+    const isPasswordValid = await user.passwordValidator(password)
     if (isPasswordValid) {
       // create jwt token
-      const token = await jwt.sign({ userId: user._id }, "DEV@tinder$");
+      const token = await user.getJWT();
       // store it in cookie
 
       res.cookie("token", token);
@@ -53,83 +52,23 @@ app.post("/login", async (req, res) => {
   }
 });
 
-// get user data via email
-app.get("/user", async (req, res) => {
-  try {
-    const user = await User.findOne({ email: req.body.email });
-    if (!user) {
-      res.status(404).send("User not found");
-    } else {
-      res.send(user);
-    }
-  } catch (err) {
-    res.status(400).send("Something went wrong:" + err);
-  }
-});
-
 //get profile data
-app.get("/profile", async (req, res) => {
+app.get("/profile", userAuth, async (req, res) => {
   try {
-    const cookie = req.cookies;
-    const { token } = cookie;
-    if (!token) throw new Error("Please login.");
-    const data = await jwt.verify(token, "DEV@tinder$");
-    if (!data) throw new Error("Please login.");
-    const { userId } = data;
-    if (!userId) throw new Error("User not found.");
-    const user = await User.findById({ _id: userId });
+    const user = req.user;
     res.send(user);
   } catch (err) {
     res.status(400).send("Cannot get your profile:" + err.message);
   }
 });
 
-// get all user data
-app.get("/feed", async (req, res) => {
-  try {
-    const users = await User.find({});
-    res.send(users);
-  } catch (err) {
-    res.status(400).send("Something went wrong :" + err);
+app.get('/sendConnectionRequest',userAuth,(req,res)=>{
+  try{
+    res.send(req.user.firstName+" req send")
+  }catch(err){
+    res.status(400).send("Cannot get your profile:" + err.message);
   }
-});
-
-app.delete("/user", async (req, res) => {
-  try {
-    await User.findByIdAndDelete(req.body.userId);
-    res.send("User deleted successfully.");
-  } catch (err) {
-    res.status(400).send("Something went Wrong" + err.message);
-  }
-});
-
-app.patch("/user/:userId", async (req, res) => {
-  try {
-    const userId = req.params?.userId;
-    const ALLOWED_UPDATE = ["skills", "photo", "about", "gender", "age"];
-
-    const isUpdateAllow = Object.keys(req.body).every((k) =>
-      ALLOWED_UPDATE.includes(k)
-    );
-    if (!isUpdateAllow) {
-      throw new Error("Invalid field passed");
-    }
-    if (req.body?.skills?.length > 10) {
-      throw new Error("Cannot add more than 10 skills");
-    }
-    if (req.body?.about?.length > 200) {
-      throw new Error("About cannot be more than 200 characters");
-    }
-
-    await User.findByIdAndUpdate(userId, req.body, {
-      // strict: true,
-      runValidators: true,
-    });
-    res.send("User updated successfully.");
-  } catch (err) {
-    res.status(400).send("Update failed :" + err.message);
-  }
-});
+})
 
 connectDb()
   .then(() => {
--- src/models/User.js
+++ src/models/User.js
@@ -1,71 +1,91 @@
 const mongoose = require("mongoose");
-const validator = require("validator")
-const userSchema = new mongoose.Schema({
-    firstName : {
-        type : String,
-        required : true,
-        minLength : 4,
-        maxLength : 50
-    },
-    lastName : {
-        type : String,
-        minLength : 4,
-        maxLength : 50
-    },
-    email : {
-        type : String,
-        required: true,
-        unique: true ,
-        trim : true,
-        lowercase : true,
-        validate(val){
-            if(!validator.isEmail(val)){
-                throw new Error("Enter a valid email.")
-            }
-        }
-    },
-    password : {
-        type : String,
-        required: true,
-        validate(val){
-            if(!validator.isStrongPassword(val)){
-                throw new Error("Your password is not strong")
-            }
-        }
-    },
-    age : {
-        type : Number,
-        min: 18
-    },
-    skills : {
-        type : [String]
-    },
-    photo : {
-        type : String,
-        validate(val){
-            if(!validator.isURL(val)){
-                throw new Error("Enter a valid image url.")
-            }
-        }
-    },
-    about : {
-        type : String,
-        default : "Hey, I am a developer."
-    },
-    gender : {
-        type : String,
-        lowercase : true,
-        validate(val){
-            if(!['male','female','other'].includes(val.toLowerCase())){
-                throw new error('Gender is Invalid')
-            }
-        },
-    }
-},
-{
-    timestamps : true
-})
+const validator = require("validator");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
 
-const User = mongoose.model("User",userSchema);
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      minLength: 4,
+      maxLength: 50,
+    },
+    lastName: {
+      type: String,
+      minLength: 4,
+      maxLength: 50,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
+      validate(val) {
+        if (!validator.isEmail(val)) {
+          throw new Error("Enter a valid email.");
+        }
+      },
+    },
+    password: {
+      type: String,
+      required: true,
+      validate(val) {
+        if (!validator.isStrongPassword(val)) {
+          throw new Error("Your password is not strong");
+        }
+      },
+    },
+    age: {
+      type: Number,
+      min: 18,
+    },
+    skills: {
+      type: [String],
+    },
+    photo: {
+      type: String,
+      validate(val) {
+        if (!validator.isURL(val)) {
+          throw new Error("Enter a valid image url.");
+        }
+      },
+    },
+    about: {
+      type: String,
+      default: "Hey, I am a developer.",
+    },
+    gender: {
+      type: String,
+      lowercase: true,
+      validate(val) {
+        if (!["male", "female", "other"].includes(val.toLowerCase())) {
+          throw new error("Gender is Invalid");
+        }
+      },
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
-module.exports = User
+userSchema.methods.getJWT = async function () {
+  const user = this;
+  const token = await jwt.sign({ userId: user._id }, "DEV@tinder$");
+  return token;
+};
+
+userSchema.methods.passwordValidator = async function (passwordInputByUser) {
+  const user = this;
+  const isPasswordValid = await bcrypt.compare(
+    passwordInputByUser,
+    user.password
+  );
+  return isPasswordValid;
+};
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
